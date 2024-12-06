a7bb8dd9d0422caef1084365c08e7157ac6968ed
rey2424 <reyrey2424@icloud.com>
2024-12-06 03:13:17 +0000

    initial commit

---
+++ css/styles.cs
@@ -0,0 +1,18 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: red;
+    font-weight: 10px;
+    align-items: center;
+}
+
+#countdown {
+    text-align: center;
+    font-weight: 10px;
+
+}
+
+#timer {
+    font-size: 50px;
+  color: green;
+  font-weight: 10px;
+}
